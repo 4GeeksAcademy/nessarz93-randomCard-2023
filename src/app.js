e6cfb1ff7555d48b49c4d4de0f34eb98641d7a98
Vanessa <126210620+nessarz93@users.noreply.github.com>
2023-09-16 01:35:25 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const simbolo = ["♦", "♥", "♠", "♣"];
+  const color = ["red", "black"];
+  let simboloAleatorio = simbolo[Math.floor(Math.random() * simbolo.length)];
+  let colorAleatorio = color[Math.floor(Math.random() * color.length)];
+
+  document.getElementById("up").innerText = simboloAleatorio;
+  document.getElementById("down").innerText = simboloAleatorio;
+  document.getElementById("up").style.color = colorAleatorio;
+  document.getElementById("down").style.color = colorAleatorio;
+
+  const numeroCentral = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J"];
+  document.getElementById("numeroAleatorio").innerText =
+    numeroCentral[Math.floor(Math.random() * numeroCentral.length)];
 };
